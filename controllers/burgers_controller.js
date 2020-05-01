aa0bc5dd5248c1833fd97f2b2e42497d0b167fba
henok tilahun <henokcity@yahoo.com>
2020-05-01 11:14:01 -0400

    PUT route is added to update rows

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -30,5 +30,23 @@ router.post("/api/burgers", function(req, res) {
   });
 });
 
+//PUT route to update burger devoured state.
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+})
+
 // Export routes for server.js to use.
 module.exports = router
