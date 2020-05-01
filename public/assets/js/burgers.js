e9f7ea26f7f2cf8f2130aadbe7fe1cb52c80bd18
henok tilahun <henokcity@yahoo.com>
2020-05-01 12:12:16 -0400

    adding a click event on Devour me button and PUT it to update the row

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -24,5 +24,27 @@ $(function() {
         }
       );
     });
+
+    //Click event for "Devour me" button.
+    $(".change-devour").on("click", function(event) {
+        var id = $(this).data("id");
+        var newDevour = $(this).data("newdevour");
     
+        var newDevourState = {
+          devoured: "true"
+        };
+    
+        // Send the PUT request using ajax.
+        $.ajax("/api/burgers/" + id, {
+          type: "PUT",
+          data: newDevourState
+        }).then(
+          function() {
+            console.log("changed devour to", newDevour);
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      });
+
 });
