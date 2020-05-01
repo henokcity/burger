cd6fc98428394b00498fb232d00507ea26e9a93e
henok tilahun <henokcity@yahoo.com>
2020-05-01 11:07:52 -0400

    adding all models and initiated the GET route

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,23 @@
+//Require express
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+//GET route to get burgers from database.
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+
+// Export routes for server.js to use.
+module.exports = router
